b3bb56d8b93445485f0b9fec73d474423c8349b5
Basil <basil.jose@student.tu-freiberg.de>
2020-11-17 19:26:37 +0100

    Completed rough code modification and data generation- linear elastic

--- FEA_linear_viscoelastic/main.py
+++ FEA_linear_viscoelastic/main.py
@@ -51,6 +51,8 @@ def main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_el
     ps = 0
     p = 0
 
+    ##create an empty file
+    
     ue = np.zeros((2,1))
     um = np.zeros((num_of_elements+1,1))
     uk = np.zeros((num_of_elements+1, 1))
@@ -60,7 +62,7 @@ def main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_el
     stress_values = np.zeros((num_of_elements+1, 2))
     m = 1
     delta_strain = np.zeros((2,1))
-
+    count = 1
     for time in np.arange(delta_t,tF+delta_t,delta_t):
 
         """Looping in time to find the displacements"""
@@ -111,110 +113,75 @@ def main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_el
                 ##Elemental forces
                 f_internal_e = f_internal(B_, stress_, N_, J_ ,element_nodes)
                 f_external_e = f_external(p,a,e)
-                element_end_time = ti.time()
-                print('element_duration', (element_end_time - element_start_time), e)
+                #element_end_time = ti.time()
+                #print('element_duration', (element_end_time - element_start_time), e)
                 Kt = Kt + ((A.T)@(Kte)@(A))
                 Rk = Rk + A.T @ (f_internal_e- f_external_e)
                 ##Total Forces
                 F_ext = F_ext + A.T@(f_external_e)
                 F_int = F_int + A.T.dot(f_internal_e)
-                elaspsed_time= start_time - ti.time()
-                print('total time =', elaspsed_time)
+                #elaspsed_time= start_time - ti.time()
+                f = open('data_linear_elastic.txt', 'a+')
+                print(E,v,element_length,Q,T,p,f_internal_e[0],f_internal_e[1],f_external_e[0],f_external_e[1],Kte[0,0],Kte[0,1],Kte[1,0],Kte[1,1],sep=',', file=f)
+                f.close()
+                #print('total time =', elaspsed_time)
+                #print('count:  ', count, 'element: ', e, 'f_int:  ', f_internal_e.ravel(), 'f_ext:  ', f_external_e.ravel(), 'Kte:   ', Kte.ravel(), '\n')
+                count+=1
             delta_uk = -1 *(np.linalg.inv(Kt) @ Rk)
             um = uk + delta_uk  
             nr = nr +1
-
-            if not Infinity_norm(delta_uk) < 0.005*Infinity_norm(um) or Infinity_norm(Rk) < 0.005 * Infinity_norm(F_int): ##Exit statement as given in question
+            
+            
+            if not Infinity_norm(delta_uk) < 0.005*Infinity_norm(um) or Infinity_norm(F_int-F_ext) < 0.005 * Infinity_norm(F_int): ##Exit statement as given in question
                 break
 
         uvalues[:,m] = np.reshape(um, num_of_elements+1)  ##array of displacement values in each time step
         m = m +1
     
-    f = open('results.txt', 'w')  ##writing to output file
+    #f = open('results.txt', 'w')  ##writing to output file
 
-    print("At final loading(tf):\n\n", "Stresses\n\n\nRadial Stress(Sigma_rr):\n\n" ,stress_values[1:,0], "\n\nCircumfrential Stress(Sigma_phi):\n\n", stress_values[1:,1],  file=f)
-    print("\n\nDisplacements\n\nU(r):\n ", um.T, file=f)
-    
+    #print("At final loading(tf):\n\n", "Stresses\n\n\nRadial Stress(Sigma_rr):\n\n" ,stress_values[1:,0], "\n\nCircumfrential Stress(Sigma_phi):\n\n", stress_values[1:,1],  file=f)
+    #print("\n\nDisplacements\n\nU(r):\n ", um.T, file=f)
     if Q > 0:  ##for plotting purposes
         type_ = 'Viscoelastic'
     else:
         type_ = 'Elastic'
 
-
-    u_analytical = analytical_elastic(E,v,p,a,b,mesh(a,b,20, 2))  ##analytical solution
-    
-    fig , ax = plt.subplots()
-    ax.scatter(mesh(a,b,20, mesh_refinement_factor), u_analytical,marker='*', c='green', label="analytical")
-    ax.plot(rnodes, um, marker='+', color='orange',label="fea")
-    ax.set_xlabel('Radius(r)')
-    ax.set_ylabel('Displacement(u)')
-    plt.text(60, 0.047, f'Time Interval(s)={delta_t}')
-    plt.text(60, 0.049, f'Number of elements={num_of_elements}')
-    plt.title(f'Convergence - {type_}')
-    plt.legend()
-    plt.savefig('convergence.png')
+    #print(nr)
+    u_analytical = analytical_elastic(E,v,p,a,b,mesh(a,b,num_of_elements, 2))  ##analytical solution
+    #print(u_analytical)
+    #print((u_analytical-um.T))
+    if (abs(u_analytical-um.T)).max() > 1e-15:
+        print('inaccurate solution', abs(u_analytical-um.T).max())
+        
+    # fig , ax = plt.subplots()
+    # ax.scatter(mesh(a,b,num_of_elements, mesh_refinement_factor), u_analytical,marker='*', c='green', label="analytical")
+    # ax.plot(rnodes, um, marker='.', color='red',label="fea")
+    # ax.set_xlabel('Radius(r)')
+    # ax.set_ylabel('Displacement(u)')
+    # plt.text(60, 0.047, f'Time Interval(s)={delta_t}')
+    # plt.text(60, 0.049, f'Number of elements={num_of_elements}')
+    # plt.title(f'Convergence - {type_}')
+    # plt.legend()
+    # plt.savefig('convergence.png')
  
 
     return um, rnodes, delta_t , num_of_elements, u_analytical, nr, uvalues
 
-def convergence_elastic_study():
-    um1, nodes1, delta_t1, num_of_elements1, u_analytical, nr, uvalues=main(E, v, 0, T, a,b,p_max,tL,tF, mesh_refinement_factor, 0.5, 1)
-    um2, nodes2, delta_t2, num_of_elements2, u_analytical, nr, uvalues=main(E, v, 0, T, a,b,p_max,tL,tF, mesh_refinement_factor, 0.5, 2)
-    um3, nodes3, delta_t3, num_of_elements3, u_analytical, nr, uvalues=main(E, v, 0, T, a,b,p_max,tL,tF, mesh_refinement_factor, 0.5, 5)
-    um4, nodes4, delta_t4, num_of_elements4, u_analytical, nr, uvalues=main(E, v, 0, T, a,b,p_max,tL,tF, mesh_refinement_factor, 0.5, 10)
-
-    fig,ax = plt.subplots()
-    ax.scatter(mesh(a,b,20, 2), u_analytical,marker='*', c='black', label="analytical")
-    ax.plot(nodes1, um1, '--' ,label=f'time interval={delta_t1}, #elements={num_of_elements1}')
-    ax.plot(nodes2, um2,'--', label=f'time interval={delta_t2}, #elements={num_of_elements2}')
-    ax.plot(nodes3, um3, '--', label=f'time interval={delta_t3}, #elements={num_of_elements3}')
-    ax.plot(nodes4, um4, label=f'time interval={delta_t4}, #elements={num_of_elements4}')
-    plt.text(56, 0.0465, f'#NR runs for each time step={nr}')
-    ax.set_xlabel('Radius(r)')
-    ax.set_ylabel('Displacement(u)')
-    plt.legend()
-    plt.title('Elastic')
-    plt.savefig('convergence_study_elastic.png')
-
-def convergence_viscoelastic_study():
-    um1, nodes1, delta_t1, num_of_elements1, u_analytical, nr, uvalues=main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, 10, 2)
-    um2, nodes2, delta_t2, num_of_elements2, u_analytical, nr, uvalues=main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, 8, 4)
-    um3, nodes3, delta_t3, num_of_elements3, u_analytical, nr, uvalues=main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, 5, 5)
-    um4, nodes4, delta_t4, num_of_elements4, u_analytical, nr, uvalues=main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, 2, 15)
-
-    fig,ax = plt.subplots()
-    ax.scatter(mesh(a,b,20, 2), u_analytical, c='black',marker='*', label="analytical")
-    ax.plot(nodes1, um1, '--' ,label=f'time interval={delta_t1}, #elements={num_of_elements1}')
-    ax.plot(nodes2, um2, '--', label=f'time interval={delta_t2}, #elements={num_of_elements2}')
-    ax.plot(nodes3, um3, '--', label=f'time interval={delta_t3}, #elements={num_of_elements3}')
-    ax.plot(nodes4, um4, marker='+',label=f'time interval={delta_t4}, #elements={num_of_elements4}')
-    ax.set_xlabel('Radius(r)')
-    ax.set_ylabel('Displacement(u)')
-    plt.legend()
-    plt.title('Viscoelastic')
-    plt.savefig('convergence_study_viscoelastic.png')
-
-def pipe_widening(delta_t, num_of_elements): ##plo time history of pipe widening
-    um_elastic, nodes, delta_t_elastic, num_of_elements_elastic, u_analytical, nr, uvalues_elastic=main(E, v, 0, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_elements)
-    um, nodes, delta_t, num_of_elements, u_analytical, nr, uvalues=main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_elements)
-    fig, ax2 = plt.subplots()
-    ax2.plot(np.arange(0,tF+delta_t,delta_t), uvalues[-1,:], label="Viscoelastic", color='orange')
-    ax2.plot(np.arange(0,tF+delta_t,delta_t), uvalues_elastic[-1,:],'--', label="Elastic", color='black')
-    ax2.set_xlabel('Time(t)')
-    ax2.set_ylabel('Displacement(u)')
-    plt.text(10, 0.007, f'Time Interval(s)={delta_t}')
-    plt.text(10, 0.009, f'Number of elements={num_of_elements}')
-    plt.title('Time history of pipe widening')
-    plt.legend()
-    plt.savefig('widening.png')
 
 
-num_of_elements=50
-delta_t = 2
+num_of_elements=5
+delta_t = 1
 #convergence_elastic_study()
 #convergence_viscoelastic_study()
 #pipe_widening(delta_t, num_of_elements)
 
-main(E, v, Q*0, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_elements)  ##elastic
+#main(E, v, Q*0, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_elements)  ##elastic
 #main(E, v, Q, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_elements)  ##viscoelastic
-
+f= open('data_linear_elastic.txt', 'w')
+f.truncate(0)
+f.write('E,v,element_length,Q,T,p,f_int[0],f_int[1],f_ext[0],f_ext[1],kte[0,0],kte[0,1],kte[1,0],kte[1,1]\n')
+f.close()
+for E in range(50000, 400000+50000, 50000):
+    for v in np.arange(0.2, 0.5, 0.05):
+        main(E, v, Q*0, T, a,b,p_max,tL,tF, mesh_refinement_factor, delta_t, num_of_elements)
